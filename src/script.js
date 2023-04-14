626ec941461ff28a5fd405bf183b548d098d5d73
im-helen <127785856+im-helen@users.noreply.github.com>
2023-04-14 00:22:22 -0400

    Added script to show current temperature and time

---
+++ src/script.js
@@ -0,0 +1,59 @@
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let hours = ("0" + now.getHours()).slice(-2);
+  let minutes = ("0" + now.getMinutes()).slice(-2);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let date = now.getDate();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[now.getMonth()];
+  let year = now.getFullYear();
+
+  return `${day} ${month} ${date}, ${year} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.city;
+  document.querySelector("#current-temp-display").innerHTML = Math.round(
+    response.data.temperature.current
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.temperature.feels_like
+  );
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.time * 1000
+  );
+}
+
+let apiKey = "56a8a52fdb532o964ddd1d934709d5bt";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
