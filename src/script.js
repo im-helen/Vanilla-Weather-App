0b06edc3350bd30f0a7514482990fe6f5d785492
im-helen <127785856+im-helen@users.noreply.github.com>
2023-04-14 00:40:03 -0400

    Updated to show API icon for current weather section

--- src/script.js
+++ src/script.js
@@ -51,9 +51,16 @@ function displayTemperature(response) {
   document.querySelector("#date").innerHTML = formatDate(
     response.data.time * 1000
   );
+  document
+    .querySelector("#current-temp-icon")
+    .setAttribute("src", response.data.condition.icon_url);
+  document
+    .querySelector("#current-temp-icon")
+    .setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "56a8a52fdb532o964ddd1d934709d5bt";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
